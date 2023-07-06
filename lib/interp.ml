a2bc007fd86171710caa962478823619653d47b2
cosine <trash@cosine.online>
2023-07-06 12:04:01 -0700

    Use values instead of auxiliary expressions

--- lib/interp.ml
+++ lib/interp.ml
@@ -6,6 +6,7 @@ open Base.Exn
    to more closely match my paper rules.  CEK is a future optimization *)
 
 exception Todo
+exception Impossible
 
 type loc = string
 [@@deriving show]
@@ -23,15 +24,20 @@ type vnop =
 type value = place * vnop
 [@@deriving show]
 
-type env = (id * exp) list
+type ctrl =
+  | Exp of exp
+  | Val of value
 [@@deriving show]
 
-(* Can i use values / place values somehow here? *)
+type env = (id * value) list
+[@@deriving show]
+
 type ctx =
   | Fun of place * id * exp * env
   | Arg of exp
-  | Flds of place * (label * exp) list * label * (label * exp) list
+  | Flds of place * (label * vnop) list * label * (label * exp) list
   | Lbl of label
+  | RefP of place
 [@@deriving show]
 
 type cont = ctx list
@@ -49,36 +55,40 @@ let is_v e = not @@ does_raise (fun () -> v_of_e e)
 let send p q c e k =
   (* i want to use an effect for this but i don't have ocaml 5 *)
   print_endline @@ "MESSAGE (" ^ show_place p ^ "->" ^ show_place q ^ "): ("
-    ^ show_exp c ^ ", " ^ show_env e ^ ", " ^ show_cont k ^ ")"
+    ^ show_ctrl c ^ ", " ^ show_env e ^ ", " ^ show_cont k ^ ")"
 
-let step (c, ((env_p, binds) as env), s, k) = match (c, k) with
+let step (c, env, s, k) = match (c, k) with
   (* i don't like that this is None... but no "step" really happened! *)
-  | (App (e1, e2), k) -> (None, (e1, env, s, Arg e2 :: k))
-  | (Lam (p, x, _, e1), Arg e2 :: k) -> (Some p, (e2, env, s, Fun (p, x, e1, binds) :: k))
-  | (ve, Fun (p, x, e, env) :: k) -> (Some p, (e, (p, (x, ve) :: env), s, k))
-  | (Id x, k) -> (Some env_p, (List.assoc x binds, env, s, k))
-  | (Rcd (p, (l, e) :: es), k) when not (is_v c) -> (Some p, (e, env, s, Flds (p, [], l, es) :: k))
-  | (ve, Flds (p, vs, vl, (l, e) :: es) :: k) -> (Some p, (e, env, s, Flds (p, (vl, ve) :: vs, l, es) :: k))
-  | (ve, Flds (p, vs, vl, []) :: k) -> (Some p, (Rcd (p, (vl, ve) :: vs), env, s, k))
-  | (Fld (e, l), k) -> (None, (e, env, s, Lbl l :: k))
-  | (Rcd (p, es), Lbl l :: k) -> (Some p, (List.assoc l es, env, s, k))
-  | _ -> raise Todo
+  | (Exp (True p), k) -> (Some p, (Val (p, T), env, s, k))
+  | (Exp (False p), k) -> (Some p, (Val (p, F), env, s, k))
+  | (Exp (App (e1, e2)), k) -> (None, (Exp e1, env, s, Arg e2 :: k))
+  | (Exp (Lam (p, x, _, e1)), Arg e2 :: k) -> (Some p, (Exp e2, env, s, Fun (p, x, e1, env) :: k))
+  | (Val v, Fun (p, x, e, env) :: k) -> (Some p, (Exp e, (x, v) :: env, s, k))
+  | (Exp (Id x), k) -> let (p, v) = List.assoc x env in (Some p, (Val (p, v), env, s, k))
+  | (Exp (Rcd (p, (l, e) :: es)), k) -> (Some p, (Exp e, env, s, Flds (p, [], l, es) :: k))
+  | (Val (_, v), Flds (p, vs, vl, (l, e) :: es) :: k) -> (Some p, (Exp e, env, s, Flds (p, (vl, v) :: vs, l, es) :: k))
+  | (Val (_, v), Flds (p, vs, vl, []) :: k) -> (Some p, (Val (p, RcdV ((vl, v) :: vs)), env, s, k))
+  | (Exp (Fld (e, l)), k) -> (None, (Exp e, env, s, Lbl l :: k))
+  | (Val (p, RcdV es), Lbl l :: k) -> (Some p, (Val (p, List.assoc l es), env, s, k))
+  | _ ->
+    print_endline ("unmatched: (" ^ show_ctrl c ^ ", " ^ show_env env ^ ", " ^ show_cont k ^ ")");
+    raise Todo
 
 let eval e =
   let rec ev p cesk = match cesk with
-    | (c, e, s, []) when is_v c -> (c, e, s, [])
+    | (Val v, e, s, []) -> (v, e, s, [])
     | _ ->
       let (q, cesk) = step cesk in
       match (p, q) with
         | (p, Some p') when p = p' -> ev p cesk
         | (p, None) -> ev p cesk
         | (p, Some q) ->
-          let (c, (_, e), _, k) = cesk in
+          let (c, e, _, k) = cesk in
           send p q c e k;
           ev q cesk in
   let starting_placement = Named "THE MOTHERFISH" in
-  let (ve, _, _, _) = ev starting_placement (e, (starting_placement, []), [], []) in
-  v_of_e ve
+  let (v, _, _, _) = ev starting_placement (Exp e, [], [], []) in
+  v
 
 let c = Named "Client"
 let s = Named "Server"
